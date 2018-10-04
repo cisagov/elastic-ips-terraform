b5bf22a07c1ce01e1e41b6ce76faa471f54a5bac
Dave Redmin <david.redmin@beta.dhs.gov>
2018-10-04 15:43:57 -0400

    Add new variable to control the number of elastic IPs to create per AWS region.

--- variables.tf
+++ variables.tf
@@ -3,9 +3,13 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default = "a"
+variable "aws_eips_per_region" {
+  type = "map"
+  description = "The number of elastic IPs to create per AWS region."
+  default = {}
+  # NOTE: Currently limited to: us-east-1, us-east-2, us-west-1, us-west-2
+  # Example:
+  #  aws_regions = { us-east-1 = 4, us-east-2 = 4, us-west-1 = 4, us-west-2 = 4 }
 }
 
 variable "tags" {
