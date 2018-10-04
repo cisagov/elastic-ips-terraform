8c91f649abe6ffff9bfe8f8a478cd0f1ef88e72b
Dave Redmin <david.redmin@beta.dhs.gov>
2018-10-04 15:42:48 -0400

    Add provider aliases for the four US AWS regions

--- main.tf
+++ main.tf
@@ -3,5 +3,25 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias = "us-west-1"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias = "us-west-2"
+  region = "us-west-2"
+}
+
 # The AWS account ID being used
 data "aws_caller_identity" "current" {}
