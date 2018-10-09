a357a94c56db29e3a3b876647641001fc91992b8
Dave Redmin <david.redmin@beta.dhs.gov>
2018-10-09 09:21:53 -0400

    Give each EIP a unique name

--- cyhy_eip.tf
+++ cyhy_eip.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "cyhy_us_east_1" {
   # us-east-1 is the default provider, so there is no need to specify
   # a provider here
   vpc = true
-  tags = "${merge(var.tags, map("Name", "CyHy EIP (us-east-1)"))}"
+  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-east-1_%d", count.index+1)))}"
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -17,7 +17,7 @@ resource "aws_eip" "cyhy_us_east_2" {
   count = "${var.aws_eips_per_region["us-east-2"]}"
   provider = "aws.us-east-2"
   vpc = true
-  tags = "${merge(var.tags, map("Name", "CyHy EIP (us-east-2)"))}"
+  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-east-2_%d", count.index+1)))}"
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -30,7 +30,7 @@ resource "aws_eip" "cyhy_us_west_1" {
   count = "${var.aws_eips_per_region["us-west-1"]}"
   provider = "aws.us-west-1"
   vpc = true
-  tags = "${merge(var.tags, map("Name", "CyHy EIP (us-west-1)"))}"
+  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-west-1_%d", count.index+1)))}"
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -43,7 +43,7 @@ resource "aws_eip" "cyhy_us_west_2" {
   count = "${var.aws_eips_per_region["us-west-2"]}"
   provider = "aws.us-west-2"
   vpc = true
-  tags = "${merge(var.tags, map("Name", "CyHy EIP (us-west-2)"))}"
+  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-west-2_%d", count.index+1)))}"
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
