703597633f5fb4e61af1270c20cf97a3aa8fa760
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-06 17:04:16 -0400

    Run terraform 0.12upgrade
    These changes are all from running terraform 0.12upgrade script.

--- cyhy_eip.tf
+++ cyhy_eip.tf
@@ -1,10 +1,16 @@
 # TODO: Find a good way to reduce the duplication here
 resource "aws_eip" "cyhy_us_east_1" {
-  count = "${var.aws_eips_per_region["us-east-1"]}"
+  count = var.aws_eips_per_region["us-east-1"]
+
   # us-east-1 is the default provider, so there is no need to specify
   # a provider here
   vpc = true
-  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-east-1_%d", count.index+1)))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("CyHy EIP us-east-1_%d", count.index + 1)
+    },
+  )
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -14,10 +20,15 @@ resource "aws_eip" "cyhy_us_east_1" {
 }
 
 resource "aws_eip" "cyhy_us_east_2" {
-  count = "${var.aws_eips_per_region["us-east-2"]}"
-  provider = "aws.us-east-2"
-  vpc = true
-  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-east-2_%d", count.index+1)))}"
+  count    = var.aws_eips_per_region["us-east-2"]
+  provider = aws.us-east-2
+  vpc      = true
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("CyHy EIP us-east-2_%d", count.index + 1)
+    },
+  )
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -27,10 +38,15 @@ resource "aws_eip" "cyhy_us_east_2" {
 }
 
 resource "aws_eip" "cyhy_us_west_1" {
-  count = "${var.aws_eips_per_region["us-west-1"]}"
-  provider = "aws.us-west-1"
-  vpc = true
-  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-west-1_%d", count.index+1)))}"
+  count    = var.aws_eips_per_region["us-west-1"]
+  provider = aws.us-west-1
+  vpc      = true
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("CyHy EIP us-west-1_%d", count.index + 1)
+    },
+  )
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -40,10 +56,15 @@ resource "aws_eip" "cyhy_us_west_1" {
 }
 
 resource "aws_eip" "cyhy_us_west_2" {
-  count = "${var.aws_eips_per_region["us-west-2"]}"
-  provider = "aws.us-west-2"
-  vpc = true
-  tags = "${merge(var.tags, map("Name", format("CyHy EIP us-west-2_%d", count.index+1)))}"
+  count    = var.aws_eips_per_region["us-west-2"]
+  provider = aws.us-west-2
+  vpc      = true
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("CyHy EIP us-west-2_%d", count.index + 1)
+    },
+  )
 
   # We want these EIPs to stick around for a long time so that our outbound
   # IP footprint does not change very often
@@ -51,3 +72,4 @@ resource "aws_eip" "cyhy_us_west_2" {
     prevent_destroy = true
   }
 }
+
